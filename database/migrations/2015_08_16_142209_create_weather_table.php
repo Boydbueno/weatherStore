b955f0a90b2e440c6d992998b6f9dc022cb4abd3
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2015-08-16 17:45:36 +0200

    Created weather migration

---
+++ database/migrations/2015_08_16_142209_create_weather_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWeatherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('lat');
+            $table->string('lon');
+            $table->string('type');
+            $table->string('temp');
+            $table->dateTime('dt');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('weather');
+    }
+}
