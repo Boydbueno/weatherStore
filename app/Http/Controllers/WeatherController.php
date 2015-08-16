c2eccac0ada601321b9f0156a61e01ae849b9807
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2015-08-16 17:51:12 +0200

    Retrieve new weather from webservice and store it

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -21,6 +21,22 @@ class WeatherController extends BaseController
         $lat = $input['lat'];
         $lon = $input['lon'];
 
+        // We grab the latest data from this lat and long
+        $rawData = file_get_contents('http://api.openweathermap.org/data/2.5/weather?lat=' . $lat . '&lon=' . $lon);
+        $jsonData = json_decode($rawData, true);
+
+        // We transform this data
+        $data = [
+            'dt' => $jsonData['dt'],
+            'lat' => $lat,
+            'lon' => $lon,
+            'type' => $jsonData['weather'][0]['main'],
+            'temp' => $jsonData['main']['temp'] - 273.15,
+        ];
+
+            // We store the data in our database
+            $weather->create($data);
+
         // We grab the last 10 weather report from given lat/lon and return it
         return $weather->where('lat', $lat)->where('lon', $lon)->limit(10)->get();
     }
